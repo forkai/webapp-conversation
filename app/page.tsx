27242a0e6e0473019a338cc3c03eed45a1ae5338
minyihong <yhmin@isoftstone.com>
2025-02-19 22:55:06 +0800

    背景

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import Icon from '@/public/image/roboticon.png'
+import background from '@/public/image/mainBackground.png'
+
 
 const Card = ({ title, desc, icon, type }: any) => {
   return (
     <Link href={{ pathname: '/chat', query: { type, title } }} className='flex flex-col justify-between shadow-md p-6 m-4 bg-white rounded-lg h-40 hover:shadow-[0_0_0_4px_#2ca5fd] cursor-pointer'>
       <div className='flex justify-between'>
-        <div className='font-semibold text-2xl'> {title} </div>
+        <div className='text-2xl font-semibold'> {title} </div>
         <div className='icon'>{icon}</div>
       </div>
       <div className='desc'>{desc}</div>
@@ -42,19 +44,21 @@ const App = () => {
       type: 'service',
     },
   ]
+
+
   return (
-    <div className="h-full container mx-auto max-w-3xl flex flex-col justify-between">
-      <div className='flex-1 flex flex-col items-center justify-center'>
-        <p className='flex items-center text-2xl font-bold mb-10 gap-4'>
+    <div className="container flex flex-col justify-between h-full max-w-3xl mx-auto bg-center bg-cover" style={{ backgroundImage: "url('@/public/image/mainBackground.png')" }}>
+      <div className='flex flex-col items-center justify-center flex-1'>
+        <p className='flex items-center gap-4 mb-10 text-2xl font-bold'>
           <Image src={Icon} alt='icon' />
-          <span className='font-semibold text-5xl'>物流行业大模型</span>
+          <span className='text-5xl font-semibold'>物流行业大模型</span>
         </p>
         <div className='grid grid-cols-2'>
           {list.map((item, index) => (
             <Card key={index} {...item} />))}
         </div>
       </div>
-      <p className='text-center mb-10'>COPYRIGHT @ 2023-2025 湖北XXX有限公司 ALL RIGHT RERSERVED</p>
+      <p className='mb-10 text-center'>COPYRIGHT @ 2023-2025 湖北XXX有限公司 ALL RIGHT RERSERVED</p>
     </div>
   )
 }
