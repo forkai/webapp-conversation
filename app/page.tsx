ee35d4bf8917b62e46ed97f77d1a1f97497a5965
minyihong <yhmin@isoftstone.com>
2025-02-20 12:10:02 +0800

    湖北物流

--- app/page.tsx
+++ app/page.tsx
@@ -22,8 +22,8 @@ const Card: React.FC<CardProps> = ({ title, desc, icon, type }) => {
   return (
     <Link href={{ pathname: '/chat', query: { type, title } }}
       className='flex flex-col justify-between shadow-md p-6 m-4 bg-white rounded-lg h-40 hover:shadow-[0_0_0_4px_#2ca5fd] cursor-pointer'>
-      <div className='flex justify-between items-center'>
-        <div className='font-semibold text-2xl'> {title} </div>
+      <div className='flex items-center justify-between'>
+        <div className='text-2xl font-semibold'> {title} </div>
         <Image src={icon} alt='icon' />
       </div>
       <div className='desc'>{desc}</div>
@@ -33,13 +33,13 @@ const Card: React.FC<CardProps> = ({ title, desc, icon, type }) => {
 
 const SideBar: React.FC = () => {
   return (
-    <div className='w-40 fixed left-0 p-8 bg-white/40 h-full flex flex-col items-center justify-between'>
+    <div className='fixed left-0 flex flex-col items-center justify-between w-40 h-full p-8 bg-white/40'>
       <div className='flex flex-col items-center justify-center'>
         <Image src={Icon} alt='icon' />
-        <div className='text-xl text-center'>物流行业大模型</div>
+        <div className='text-xl text-center'>湖北物流行业大模型</div>
       </div>
 
-      <div className='flex flex-1 flex-col justify-start p-20'>
+      <div className='flex flex-col justify-start flex-1 p-20'>
         <Link href={{ pathname: '/chat' }}
           className='flex flex-col items-center justify-center cursor-pointer'>
           <Image src={newdialogue} alt='icon' />
@@ -83,25 +83,25 @@ const App = () => {
     },
     {
       title: '智能客服',
-      desc: '实现24/7在线客服，推荐解决方案，提高处理效率。',
+      desc: '实现24/7在线客服，数据报告查询，推荐解决方案，提高处理效率',
       icon: customer,
       type: 'service',
     },
   ]
   return (
-    <div className="h-full container mx-auto max-w-4xl flex flex-col justify-between">
+    <div className="container flex flex-col justify-between h-full max-w-4xl mx-auto">
       <SideBar className='fixed left-0' />
-      <div className='flex-1 flex flex-col items-center justify-center'>
-        <p className='flex items-center text-2xl font-bold mb-10 gap-4'>
+      <div className='flex flex-col items-center justify-center flex-1'>
+        <p className='flex items-center gap-4 mb-10 text-2xl font-bold'>
           <Image src={Icon} alt='icon' />
-          <span className='text-6xl text-[#0D2855]'>物流行业大模型</span>
+          <span className='text-6xl text-[#0D2855]'>湖北物流行业大模型</span>
         </p>
         <div className='grid grid-cols-2'>
           {list.map((item, index) => (
             <Card key={index} {...item} />))}
         </div>
       </div>
-      <p className='text-center mb-10 text-sm'>COPYRIGHT © 2023-2025 湖北供应链物流公共信息服务股份有限公司 ALL RIGHTS RESERVED</p>
+      <p className='mb-10 text-sm text-center'>COPYRIGHT © 2023-2025 湖北供应链物流公共信息服务股份有限公司 ALL RIGHTS RESERVED</p>
     </div>
   )
 }
